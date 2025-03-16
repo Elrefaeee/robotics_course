fef59c7cb31ebd689b1ba2f66126977052d0512b
Elrefaeee <youssefelrefaeee@gmail.com>
2025-03-16 19:01:19 +0200

    Fixed embedded git repository issue

---
+++ src/robotics_course/github_test/build/github_test/CMakeFiles/github_test_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/github_test_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/github_test_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/robotics_course/github_test/build/github_test/CMakeFiles/github_test_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for github_test_uninstall.
+# This may be replaced when dependencies are built.
